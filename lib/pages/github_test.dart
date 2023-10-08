d333da5303778c12ae1e6b8ca07e2289c4ffd18a
azizbek <azizmurotov1272@gmail.com>
2023-10-08 12:46:37 +0500

    github test add

---
+++ lib/pages/github_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class GitHubTest extends StatefulWidget {
+  const GitHubTest({super.key});
+
+  @override
+  State<GitHubTest> createState() => _GitHubTestState();
+}
+
+class _GitHubTestState extends State<GitHubTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
